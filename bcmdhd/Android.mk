33eda1c88a28d60f613298b19cb2573f7a5a6dcf
Chih-Wei Huang <cwhuang@linux.org.tw>
2015-10-10 02:49:00 +0800

    Re-use lib_driver_cmd_bcmdhd
    Rename it to private_lib_driver_cmd for android-x86.

--- bcmdhd/Android.mk
+++ bcmdhd/Android.mk
@@ -1,3 +1,5 @@
 ifeq ($(BOARD_WLAN_DEVICE),bcmdhd)
     include $(call all-subdir-makefiles)
+else ifneq ($(filter x86%,$(TARGET_ARCH)),)
+    include $(call all-named-subdir-makefiles,wpa_supplicant_8_lib)
 endif
